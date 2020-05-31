0e3e029563081c71167eac4b1b33957b20942a87
Maxim Dick <maxim1994barca@gmail.com>
2020-05-31 15:18:04 +0300

    2.1. Преобразование даты [#289476]

---
+++ src/main/java/ru/job4jgrabber/SqlRuDateParse.java
@@ -0,0 +1,33 @@
+package ru.job4jgrabber;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SqlRuDateParse {
+
+    /**
+     * Конвертирует дату из строки в Date.
+     * */
+    public static Date convertDate(String stringDate) {
+        Date date = null;
+        String[] givenMonths = {"янв", "фев", "мар", "апр", "май", "июн",
+                "июл", "авг", "сен", "окт", "ноя", "дек"};
+        String[] realMonths = {"01", "02", "03", "04", "05", "06",
+                "07", "08", "09", "10", "11", "12"};
+
+        for (int i = 0; i < givenMonths.length; i++) {
+            stringDate = stringDate.replace(givenMonths[i], realMonths[i]);
+        }
+
+        try {
+            DateFormat format = new SimpleDateFormat("dd MM yy, HH:mm");
+            date = format.parse(stringDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+}
